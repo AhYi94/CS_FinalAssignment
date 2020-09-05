8f39b911c61cc6512e8b2a7af25a1355bbc9f42c
Yung Gie <lingyunggie@gmail.com>
2020-09-05 13:15:20 +0800

    Login Template Imported

--- FinalAssignment/FinalAssignment/login.aspx.designer.cs
+++ FinalAssignment/FinalAssignment/login.aspx.designer.cs
@@ -13,32 +13,5 @@ namespace FinalAssignment
 
     public partial class login
     {
-
-        /// <summary>
-        /// username control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox username;
-
-        /// <summary>
-        /// password control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox password;
-
-        /// <summary>
-        /// loginButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button loginButton;
     }
 }
