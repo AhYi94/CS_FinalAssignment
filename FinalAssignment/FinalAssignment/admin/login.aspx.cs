1a601502a555f5c1539050ce4e9adab1e9a3e429
Andrew Wong <andrewwong82023@gmail.com>
2020-09-06 13:57:12 +0800

    add login validation

--- FinalAssignment/FinalAssignment/admin/login.aspx.cs
+++ FinalAssignment/FinalAssignment/admin/login.aspx.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace FinalAssignment.admin
 {
     public partial class login : System.Web.UI.Page
     {
+
+        DatabaseConnection dbcon = new DatabaseConnection();
+
+        DataTable dt = new DataTable();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void login_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = "select * from admins where email='" + email.Text + "' and password='" + password.Text + "';"; ;
+                dbcon.executeSQL(query);
+                dt = dbcon.getDataSQL(query);
+
+                if (dt.Rows.Count > 0)
+                {
+           
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Login Successful')", true);
+                    Response.Redirect("index.aspx");
+                }
+                else
+                {
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Login Fail')", true);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
