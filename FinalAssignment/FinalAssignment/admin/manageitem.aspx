3552d966327ac1b72aaa60076e9cae7434e0fc21
Andrew Wong <andrew.wong@minifysolution.com>
2020-09-16 13:08:55 +0800

    add delete aspx and delete function

--- FinalAssignment/FinalAssignment/admin/manageitem.aspx
+++ FinalAssignment/FinalAssignment/admin/manageitem.aspx
@@ -2,5 +2,48 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Manage Item</h1>
+
+    <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Manage Item</h1>
+			</div>
+		</div><!--/.row-->	
+		
+		<div class="panel panel-default">
+					<div class="panel-heading">
+						View Product
+						<ul class="pull-right panel-settings panel-button-tab-right">
+							<li class="dropdown"><a class="pull-right dropdown-toggle" data-toggle="dropdown" href="#">
+								<em class="fa fa-cogs"></em>
+							</a>
+								<ul class="dropdown-menu dropdown-menu-right">
+									<li>
+										<ul class="dropdown-settings">
+											<li><a href="manageitem.aspx">
+												<em class="fa fa-cog"></em> View Product
+											</a></li>
+											<li class="divider"></li>
+											<li><a href="manageaddItem.aspx">
+												<em class="fa fa-cog"></em> Add Product
+											</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+						</ul>
+						<span class="pull-right clickable panel-toggle panel-button-tab-left"><em class="fa fa-toggle-up"></em></span></div>
+					<div class="panel-body">
+						<form class="form-horizontal" action="" method="post">
+							<fieldset>
+								<div class="text-center">
+                                <asp:Panel ID="Panel1" runat="server"></asp:Panel>
+								</div>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+	</div>	<!--/.main-->
+
+
 </asp:Content>
--- FinalAssignment/FinalAssignment/admin/manageitem.aspx.cs
+++ FinalAssignment/FinalAssignment/admin/manageitem.aspx.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,8 +11,31 @@ namespace FinalAssignment.admin
 {
     public partial class manageitem : System.Web.UI.Page
     {
+        private DatabaseConnection dbcon;
+
+        StringBuilder table = new StringBuilder();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+
+            dbcon = new DatabaseConnection();
+            DataTable dt = dbcon.getDataSQL("select * from products;");
+            int i = 1;
+
+            table.Append("<table border='1' style='width:100%'>");
+            table.Append("<tr><th>ID</th><th>Name</th><th>Price</th><th>Description</th><th>Image</th><th>Created_at</th><th>Update</th><th>Delete</th></tr>");
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                table.Append("<tr>");
+                table.Append("<td width=30>" + i + "</td><td width=120>" + dr[1].ToString() + "</td><td width=100>" + "RM" + dr[2].ToString() + "</td><td width=350>" + dr[3].ToString() + "</td><td width=100>" + "<img src = 'upload/" + dr[4] + "' width = 100px height = 100px >" + "</td><td width=80>" + dr[5].ToString() + "</td>");
+                table.Append("<td width=50>" + "<a href = 'adminEditProduct.aspx?id=" + dr[0] + "'><button type='button' class='btn btn - primary'>Edit</button></a>" + "</td>");
+                table.Append("<td width=50>" + "<a href = 'manageDeleteIte.aspx?id=" + dr[0] + "'><button type='button' class='btn btn - primary'>Delete</button></a>" + "</td>");
+                table.Append("</tr>");
+                i++;
+            }
+            table.Append("</table>");
+            Panel1.Controls.Add(new Label { Text = table.ToString() });
 
         }
     }
